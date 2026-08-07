ca4df24eb0b6f1c359dd61c55983e8378ef9811a
Brett Beloin <brettbeloin@gmail.com>
2026-08-06 18:54:38 -0600

    init project commit

---
+++ Ant-Colony/Program.cs
@@ -0,0 +1,13 @@
+﻿using static CSC160_ConsoleMenu.CIO;
+
+namespace Main
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            List<string> foo = new List<string> { "Hello,", "comma", "World" };
+            PromptForMenuSelection(foo, false);
+        }
+    }
+}
