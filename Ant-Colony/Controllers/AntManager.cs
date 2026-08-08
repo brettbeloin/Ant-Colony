26f715f4f7c567cee77246f5e7a926c17d366c19
Perihelion <dylanpratt707@gmail.com>
2026-08-08 15:38:42 -0600

    Added basic project setup/initial classes
    and this time im pushing it to the right place

---
+++ Ant-Colony/Controllers/AntManager.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Ant_Colony.Controllers
+{
+    internal static class AntManager
+    {
+        public static int VirtWorkerAntAmount { get; private set; }
+        public static int UsedVirtWorkerAnt {  get; private set; }
+
+    }
+}
---
+++ AntColonyAutomatedTester/UnitTest1.cs
@@ -0,0 +1,11 @@
+﻿namespace AntColonyAutomatedTester
+{
+    public class UnitTest1
+    {
+        [Fact]
+        public void Test1()
+        {
+
+        }
+    }
+}
