429a4c5cdfb268bedf3858732004b3b7652a5b5f
brett beloin <brettbeloin@gmail.com>
2026-08-11 16:47:52 -0600

    Added CombatManager

---
+++ Ant-Colony/Controllers/CombatManager.cs
@@ -0,0 +1,12 @@
+namespace Ant_Colony.Controllers;
+
+public static class CombatManager
+{
+    public static bool IsAlive { get; set; }
+    
+    static CombatManager()
+    {
+        throw new NotImplementedException();
+    }
+    
+}
