cd301cb5e02066c498006efe1af01664abfaec2f
Perihelion <dylanpratt707@gmail.com>
2026-08-11 16:06:37 -0600

    Added concrete ant classes
    They also have static members that other classes can ask about such as GatherAmount

---
+++ Ant-Colony/Models/BulletAnt.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Ant_Colony.Models
+{
+    internal class BulletAnt : BaseAnt
+    {
+    }
+}
---
+++ Ant-Colony/Models/LeafCutterAnt.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Ant_Colony.Models
+{
+    internal class LeafCutterAnt : BaseAnt
+    {
+        public static new int GatherAmount { get; } = 2;
+    }
+}
