45cdeb1826a5f2755c690e8c3fd6f11f0d2ef88c
brett beloin <brettbeloin@gmail.com>
2026-08-12 16:42:18 -0600

    updated enemy class

--- Ant-Colony/Models/Enemies.cs
+++ Ant-Colony/Models/Enemies.cs
@@ -4,8 +4,43 @@ using System.Text;
 
 namespace Ant_Colony.Models
 {
-    public class Enemies
+    public struct Stats
     {
+        public  int atk { get; set; }
+        public int def  { get; set; }
+        public int spd { get; set; };
+    }
+    
+    public abstract class Enemies
+    {
+        public Stats stats  { get; set; }
+        public string name { get; set; }
+        public int lvl { get; set; }
+        public int health { get; set; }
 
+        public Enemies()
+        {
+            stats = new Stats {atk =5, def = 5, spd = 5};
+            name = "Stinky";
+            lvl = 1;   
+            health = 10;
+        }
+
+        public Enemies(Stats stats, string name, int lvl, int health)
+        {
+            this.stats = stats;
+            this.name = name;
+            this.lvl = lvl;
+            this.health = health;
+        }
+
+        public abstract bool Attack();
+        
+        public abstract bool Defend();
+
+        public override string ToString()
+        {
+            throw new NotImplementedException("Not implemented");
+        }
     }
 }
