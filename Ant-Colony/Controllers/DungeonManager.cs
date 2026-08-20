352c846350ab157dae492ca3ce8fd9a9bee25f46
CalebDeanMcCool <calebdeanmccool@gmail.com>
2026-08-20 16:52:47 -0600

    Flushed out some dungeon rooms and added more DungeonEvent enums

--- Ant-Colony/Controllers/DungeonManager.cs
+++ Ant-Colony/Controllers/DungeonManager.cs
@@ -31,9 +31,31 @@ namespace Ant_Colony.Controllers
 
             switch(eventNum){
                 case 0:
-                    EnemySetup(eventNum);
+                    EnemySetup(1);
                     break;
                 case 1:
+                    EnemySetup(2);
+                    break;
+                case 2:
+                    EnemySetup(3);
+                    break;
+                case 3:
+                    EnemySetup(4);
+                    break;
+                case 4:
+                    EnemySetup(5);
+                    break;
+                case 5:
+                    EnemySetup(6);
+                    break;
+                case 6:
+                    EnemySetup(7);
+                    break;
+                case 7:
+                    EnemySetup(8);
+                    break;
+                case 8:
+                    EnemySetup(9);
                     break;
                 default:
                     break;
@@ -45,9 +67,47 @@ namespace Ant_Colony.Controllers
         {
             switch (eventNum)
             {
-                case 0: 
+                case 1: 
                     enemies.Add(new Beetle(new Stats() { atk=5, def=5, spd=5}, "Beetle", 1, 20, 500, false));
-                    enemies.Add(new Beetle(new Stats() { atk = 3, def = 3, spd = 3 }, "Beetle", 1, 20, 500, false));
+                    enemies.Add(new Beetle(new Stats() { atk=3, def=3, spd=3}, "Beetle", 1, 20, 500, false));
+                    break;
+                case 2: 
+                    enemies.Add(new Beetle(new Stats() { atk=5, def=5, spd=5}, "Beetle", 1, 20, 500, false));
+                    enemies.Add(new Beetle(new Stats() { atk=3, def=3, spd=3}, "Beetle", 1, 20, 500, false));
+                    enemies.Add(new Beetle(new Stats() { atk=3, def=3, spd=3}, "Beetle", 1, 20, 500, false));
+                    break;
+                case 3: 
+                    enemies.Add(new Beetle(new Stats() { atk=5, def=5, spd=5}, "Beetle", 1, 20, 500, false));
+                    enemies.Add(new Beetle(new Stats() { atk=6, def=6, spd=6}, "Beetle", 1, 20, 500, false));
+                    enemies.Add(new Beetle(new Stats() { atk=5, def=5, spd=5}, "Beetle", 1, 20, 500, false));
+                    break;
+                case 4: 
+                    enemies.Add(new Spider(new Stats() { atk=5, def=5, spd=5}, "Spider", 1, 20, 500, false));
+                    enemies.Add(new Spider(new Stats() { atk=3, def=3, spd=3}, "Spider", 1, 20, 500, false));
+                    break;
+                case 5: 
+                    enemies.Add(new Spider(new Stats() { atk=5, def=5, spd=5}, "Spider", 1, 20, 500, false));
+                    enemies.Add(new Spider(new Stats() { atk=3, def=3, spd=3}, "Spider", 1, 20, 500, false));
+                    enemies.Add(new Spider(new Stats() { atk=3, def=3, spd=3}, "Spider", 1, 20, 500, false));
+                    break;
+                case 6: 
+                    enemies.Add(new Spider(new Stats() { atk=5, def=5, spd=5}, "Spider", 1, 20, 500, false));
+                    enemies.Add(new Spider(new Stats() { atk=6, def=6, spd=6}, "Spider", 1, 20, 500, false));
+                    enemies.Add(new Spider(new Stats() { atk=5, def=5, spd=5}, "Spider", 1, 20, 500, false));
+                    break;
+                case 7: 
+                    enemies.Add(new Beetle(new Stats() { atk=4, def=4, spd=4}, "Beetle", 1, 20, 500, false));
+                    enemies.Add(new Spider(new Stats() { atk=4, def=4, spd=4}, "Spider", 1, 20, 500, false));
+                    break;
+                case 8: 
+                    enemies.Add(new Beetle(new Stats() { atk=6, def=6, spd=6}, "Beetle", 1, 20, 500, false));
+                    enemies.Add(new Spider(new Stats() { atk=6, def=6, spd=6}, "Spider", 1, 20, 500, false));
+                    break;
+                case 9: 
+                    enemies.Add(new Beetle(new Stats() { atk=5, def=5, spd=5}, "Beetle", 1, 20, 500, false));
+                    enemies.Add(new Beetle(new Stats() { atk=5, def=5, spd=5}, "Beetle", 1, 20, 500, false));
+                    enemies.Add(new Spider(new Stats() { atk=5, def=5, spd=5}, "Spider", 1, 20, 500, false));
+                    enemies.Add(new Spider(new Stats() { atk=5, def=5, spd=5}, "Spider", 1, 20, 500, false));
                     break;
             }
         }
