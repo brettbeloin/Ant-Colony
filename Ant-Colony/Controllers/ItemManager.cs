9f00d7c2490605b4bcb296793a3ded9b8a9cb3cf
Perihelion <dylanpratt707@gmail.com>
2026-08-25 15:20:29 -0600

    Stared DYLAN's item manager

--- Ant-Colony/Controllers/ItemManager.cs
+++ Ant-Colony/Controllers/ItemManager.cs
@@ -1,11 +1,33 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Text;
+using Ant_Colony.Models;
 
 namespace Ant_Colony.Controllers
 {
     public static class ItemManager
     {
+        public static List<BaseItem> GlobalInventory = new List<BaseItem>();
+        public static List<BaseItem> DungeonInventory = new List<BaseItem>();
 
+        public static void CreateDungeonInventory(int capacity)
+        {
+            DungeonInventory = new List<BaseItem>(capacity); 
+        }
+
+        public static void ClearInventories()
+        {
+            GlobalInventory.Clear();
+            DungeonInventory.Clear();
+        }
+
+        public static void MoveDungeonInventoryToGlobalInventory()
+        {
+            foreach (BaseItem item in DungeonInventory) 
+            {
+                GlobalInventory.Append(item);
+            }
+            DungeonInventory.Clear();
+        }
     }
 }
--- Ant-Colony/Models/Item.cs
+++ Ant-Colony/Models/Item.cs
@@ -4,7 +4,7 @@ using System.Text;
 
 namespace Ant_Colony.Models
 {
-    public class Item
+    public class BaseItem
     {
         public int Uses { get;
             set
